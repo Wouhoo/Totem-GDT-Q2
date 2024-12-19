bb3666a42e62eff43e2801029177b71bc431170b
Wouter <121758307+Wouhoo@users.noreply.github.com>
2024-12-19 15:53:22 +0100

    Delivery timers
    In addition to the amount of fuel to deliver, each active delivery point now also shows a timer on the minimap which counts down the remaining time for that order.

--- Frostpunk-Delivery/Assets/Scripts/DeliveryPoint.cs
+++ Frostpunk-Delivery/Assets/Scripts/DeliveryPoint.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DeliveryPoint : MonoBehaviour
 {
@@ -10,7 +11,11 @@ public class DeliveryPoint : MonoBehaviour
     public Quest quest;
 
     GameManager gameManager;
+
     TextMeshProUGUI deliveryText;
+    Image deliveryTimer;
+    Image deliveryTimerFill;
+    float remainingTime;
 
     Material pointMaterial;
     Color inactiveColor = new Color(0.7f, 0.7f, 0.7f); // gray
@@ -18,11 +23,28 @@ public class DeliveryPoint : MonoBehaviour
 
     private void Awake()
     {
+        // Find related gameobjects & set stuff up
         gameManager = FindObjectOfType<GameManager>();
-        deliveryText = GetComponentInChildren<TextMeshProUGUI>(); // has to be found before the text is parented to the canvas, hence Awake() instead of Start()
-        deliveryText.gameObject.SetActive(false);
+        deliveryText = transform.Find("DeliveryPointUI/DeliveryText").GetComponent<TextMeshProUGUI>(); // has to be found before the UI is parented to the canvas, hence Awake() instead of Start()
+        deliveryTimer = transform.Find("DeliveryPointUI/TimerBG").GetComponent<Image>();               // same here
+        deliveryTimerFill = transform.Find("DeliveryPointUI/TimerFill").GetComponent<Image>();         // and here
         pointMaterial = GetComponent<Renderer>().material;
-        pointMaterial.color = inactiveColor;
+        UISetActive(false);
+    }
+
+    private void Update()
+    {
+        if (questActive)
+        {
+            // Count down timer for active quest
+            remainingTime -= Time.deltaTime;
+            deliveryTimerFill.fillAmount = remainingTime / quest.timeLimit;
+            if(remainingTime <= 0)
+            {
+                // Fail quest if time gets <= 0
+                FailQuest();
+            }
+        }
     }
 
     public void AssignQuest(Quest newQuest)
@@ -30,21 +52,18 @@ public class DeliveryPoint : MonoBehaviour
         // Assign a new quest to this delivery point (called from QuestManager)
         quest = newQuest;
         questActive = true;
-        Invoke("FailQuest", quest.timeLimit); // Start counting down towards quest failure
-        pointMaterial.color = activeColor;
-        deliveryText.gameObject.SetActive(true);
+        remainingTime = quest.timeLimit;
+        UISetActive(true);
         deliveryText.text = string.Format("{0}", quest.fuelToDeliver); // Displays only fuel to deliver for now; might display more/other info later.
     }
 
     public void CompleteQuest()
     {
         // Complete this point's assigned quest (called from PlayerFuel)
-        CancelInvoke("FailQuest"); // Cancel failure countdown
         gameManager.UpdateScore(quest.pointReward);
         quest = null;
         questActive = false;
-        deliveryText.gameObject.SetActive(false);
-        pointMaterial.color = inactiveColor;
+        UISetActive(false);
     }
 
     void FailQuest()
@@ -53,7 +72,16 @@ public class DeliveryPoint : MonoBehaviour
         gameManager.GameOver(); // For now, failing a quest will be an instant game over
         quest = null;
         questActive = false;
-        deliveryText.gameObject.SetActive(false);
-        pointMaterial.color = inactiveColor;
+        UISetActive(false);
+    }
+
+    void UISetActive(bool active)
+    {
+        // (de)activate all UI elements
+        deliveryText.gameObject.SetActive(active);
+        deliveryTimer.gameObject.SetActive(active);
+        deliveryTimerFill.gameObject.SetActive(active);
+        if (active) pointMaterial.color = activeColor; 
+        else pointMaterial.color = inactiveColor;
     }
 }
